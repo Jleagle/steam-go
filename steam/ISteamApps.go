01cbc0d717a0f89592ec4acbb9323cfe286127f5
James Eagle <jimeagle@gmail.com>
2018-05-21 18:15:08 +0100

    Add options to GetAppList

--- steam/ISteamApps.go
+++ steam/ISteamApps.go
@@ -3,11 +3,35 @@ package steam
 import (
 	"encoding/json"
 	"net/url"
+	"strconv"
 )
 
-func (s Steam) GetAppList() (apps AppList, bytes []byte, err error) {
+type GetAppList struct {
+	ModifiedAfter int64
+	AfterAppID    int
+	MaxResults    int // 10,000 default
+}
 
-	bytes, err = s.getFromAPI("ISteamApps/GetAppList/v2", url.Values{})
+func (s Steam) GetAppList(options GetAppList) (apps AppList, bytes []byte, err error) {
+
+	q := url.Values{}
+	q.Set("include_games", "1")
+	q.Set("include_dlc", "1")
+	q.Set("include_software", "1")
+	q.Set("include_videos", "1")
+	q.Set("include_hardware", "1")
+
+	if options.ModifiedAfter > 0 {
+		q.Set("if_modified_since", strconv.FormatInt(options.ModifiedAfter, 10))
+	}
+	if options.AfterAppID > 0 {
+		q.Set("last_appid", strconv.Itoa(options.AfterAppID))
+	}
+	if options.MaxResults > 0 {
+		q.Set("max_results", strconv.Itoa(options.MaxResults))
+	}
+
+	bytes, err = s.getFromAPI("ISteamApps/GetAppList/v2", q)
 	if err != nil {
 		return apps, bytes, err
 	}
@@ -29,6 +53,8 @@ type AppList struct {
 }
 
 type App struct {
-	AppID int    `json:"appid"`
-	Name  string `json:"name"`
+	AppID             int    `json:"appid"`
+	Name              string `json:"name"`
+	LastModified      string `json:"last_modified"`
+	PriceChangeNumber string `json:"price_change_number"`
 }
