d2abc8a3cf413fb9a927936963a1c008e67c7ae3
James Eagle <jimeagle@gmail.com>
2018-10-16 13:09:20 +0100

    Update rate limit code

--- steam/steam.go
+++ steam/steam.go
@@ -2,55 +2,11 @@ package steam
 
 import (
 	"errors"
+	"go.uber.org/ratelimit"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"time"
 )
-
-type Steam struct {
-	Key        string      // API key
-	Throttle   bool        // Rate limit calls
-	LogChannel chan string // channel to return call URLs
-	Format     string      // json, vdf, xml
-	HTTP       bool
-}
-
-var (
-	apiThrottle   chan time.Time
-	storeThrottle chan time.Time
-)
-
-func init() {
-
-	// API Throttle
-	apiTick := time.NewTicker(time.Hour * 24 / 100000)
-	defer apiTick.Stop()
-
-	apiThrottle = make(chan time.Time, 1000) // Burst limit
-	go func() {
-		for t := range apiTick.C {
-			select {
-			case apiThrottle <- t:
-			default:
-			}
-		}
-	}()
-
-	// Store throttle
-	storeTick := time.NewTicker(time.Minute * 5 / 200)
-	defer storeTick.Stop()
-
-	storeThrottle = make(chan time.Time, 2) // Burst limit
-	go func() {
-		for t := range storeTick.C {
-			select {
-			case storeThrottle <- t:
-			default:
-			}
-		}
-	}()
-}
 
 var statusCodes = map[int]string{
 	400: "steam-go: (400) please verify that all required parameters are being sent.",
@@ -63,42 +19,26 @@ var statusCodes = map[int]string{
 	503: "steam-go: (503) server is temporarily unavailable, or too busy to respond. please wait and try again later.",
 }
 
-type Error struct {
-	err  string
-	code int
-}
+type Steam struct {
+	Key        string      // api key
+	LogChannel chan string // channel to return call URLs
+	RateLimit  int         // per second, 0 to disable
 
-func (e Error) Error() string {
-	return e.err
-}
-
-func (e Error) Code() int {
-	return e.code
-}
-
-func (e Error) IsHardFail() bool {
-	for _, v := range []int{400, 401, 403, 404, 405} {
-		if v == e.code {
-			return true
-		}
-	}
-	return false
+	apiThrottle   ratelimit.Limiter
+	storeThrottle ratelimit.Limiter
 }
 
 func (s Steam) getFromAPI(path string, query url.Values) (bytes []byte, err error) {
 
-	if s.Throttle {
-		<-apiThrottle
+	if s.RateLimit > 0 {
+		s.initThrottle()
+		s.apiThrottle.Take()
 	}
 
-	if s.Format == "" {
-		s.Format = "json"
-	}
-
-	query.Add("format", s.Format)
+	query.Add("format", "json")
 	query.Add("key", s.Key)
 
-	path = s.getProtocol() + "api.steampowered.com/" + path + "?" + query.Encode()
+	path = "https://api.steampowered.com/" + path + "?" + query.Encode()
 
 	if s.LogChannel != nil {
 		s.LogChannel <- path
@@ -113,28 +53,21 @@ func (s Steam) getFromAPI(path string, query url.Values) (bytes []byte, err erro
 	// Handle errors
 	if response.StatusCode != 200 {
 		if val, ok := statusCodes[response.StatusCode]; ok {
-			return bytes, Error{
-				err:  val,
-				code: response.StatusCode,
-			}
+			return bytes, Error{val, response.StatusCode}
 		} else {
 			return bytes, errors.New("steam: something went wrong")
 		}
 	}
 
 	//
-	bytes, err = ioutil.ReadAll(response.Body)
-	if err != nil {
-		return bytes, err
-	}
-
-	return bytes, nil
+	return ioutil.ReadAll(response.Body)
 }
 
 func (s Steam) getFromStore(path string, query url.Values) (bytes []byte, err error) {
 
-	if s.Throttle {
-		<-storeThrottle
+	if s.RateLimit > 0 {
+		s.initThrottle()
+		s.storeThrottle.Take()
 	}
 
 	path = "https://store.steampowered.com/" + path + "?" + query.Encode()
@@ -149,18 +82,25 @@ func (s Steam) getFromStore(path string, query url.Values) (bytes []byte, err er
 	}
 	defer response.Body.Close()
 
-	bytes, err = ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return bytes, nil
+	return ioutil.ReadAll(response.Body)
 }
 
-func (s Steam) getProtocol() string {
-	if s.HTTP {
-		return "http://"
+func (s Steam) initThrottle() {
+	if s.RateLimit > 0 && s.apiThrottle == nil {
+		s.apiThrottle = ratelimit.New(s.RateLimit)
+		s.storeThrottle = ratelimit.New(s.RateLimit)
 	}
+}
 
-	return "https://"
+type Error struct {
+	err  string
+	code int
+}
+
+func (e Error) Error() string {
+	return e.err
+}
+
+func (e Error) Code() int {
+	return e.code
 }
