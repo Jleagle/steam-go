0d5890f6b7b78855399a163709c085870fd0211b
James Eagle <jimeagle@gmail.com>
2020-02-05 20:38:39 +0000

    Redo all ID conversions

---
+++ id/id.go
@@ -0,0 +1,274 @@
+package id
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
+
+type AccountID uint32
+
+type InstanceID uint32
+
+type ChatInstanceID InstanceID
+
+//noinspection GoUnusedConst
+const (
+	ChatInstanceClan     ChatInstanceID = 0x100000 >> 1
+	ChatInstanceLobby    ChatInstanceID = 0x100000 >> 2
+	ChatInstanceMMSLobby ChatInstanceID = 0x100000 >> 3
+)
+
+type UniverseID uint8
+
+//noinspection GoUnusedConst
+const (
+	UniverseInvalid  UniverseID = 0
+	UniversePublic   UniverseID = 1
+	UniverseBeta     UniverseID = 2
+	UniverseInternal UniverseID = 3
+	UniverseDev      UniverseID = 4
+	UniverseMax      UniverseID = 5
+)
+
+type AccountType uint8
+
+//noinspection GoUnusedConst
+const (
+	AccountTypeInvalid        AccountType = 0
+	AccountTypeIndividual     AccountType = 1
+	AccountTypeMultiseat      AccountType = 2
+	AccountTypeGameServer     AccountType = 3
+	AccountTypeAnonGameServer AccountType = 4
+	AccountTypePending        AccountType = 5
+	AccountTypeContentServer  AccountType = 6
+	AccountTypeClan           AccountType = 7
+	AccountTypeChat           AccountType = 8
+	AccountTypeConsoleUser    AccountType = 9
+	AccountTypeAnonUser       AccountType = 10
+	AccountTypeMax            AccountType = 11
+)
+
+var CharacterToAccountType = map[string]AccountType{
+	"I": AccountTypeInvalid,
+	"U": AccountTypeIndividual,
+	"M": AccountTypeMultiseat,
+	"G": AccountTypeGameServer,
+	"A": AccountTypeAnonGameServer,
+	"P": AccountTypePending,
+	"C": AccountTypeContentServer,
+	"g": AccountTypeClan,
+	"T": AccountTypeChat,
+	"c": AccountTypeChat,
+	"L": AccountTypeChat,
+	"a": AccountTypeAnonUser,
+}
+
+type PlayerID uint64
+
+//noinspection RegExpRedundantEscape
+var (
+	regexpID1  = regexp.MustCompile(`^STEAM_([0-5]):([01]):([0-9]{1,8})$`)       // Universe ID, Lowest bit, Highest bit
+	regexpID3  = regexp.MustCompile(`^\[?([a-zA-Z])\:([0-5])\:(\d+)\]?$`)        // Account type character, Universe ID, Account ID
+	regexpID3I = regexp.MustCompile(`^\[?([a-zA-Z])\:([0-5])\:(\d+)\:(\d+)\]?$`) // Account type character, Universe ID, Account ID, Instance ID
+	regexpID32 = regexp.MustCompile(`^\d{1,16}$`)                                // ID
+	regexpID64 = regexp.MustCompile(`^\d{17}$`)                                  // Account ID
+)
+
+var (
+	ErrInvalidPlayerID = errors.New("invalid player id")
+	ErrInvalidClanID   = errors.New("invalid clan id")
+	ErrInvalidChatID   = errors.New("invalid chat id")
+)
+
+func ParsePlayerID(id string) (out PlayerID, err error) {
+
+	switch {
+	case regexpID1.MatchString(id):
+
+		// Get universe
+		parts := regexpID1.FindStringSubmatch(id)
+		i, err := strconv.ParseInt(parts[1], 10, 8)
+		if err != nil {
+			return out, err
+		}
+		if i == int64(UniverseInvalid) {
+			i = int64(UniversePublic)
+		}
+
+		// Get account
+		part2, err := strconv.ParseUint(parts[2], 10, 32)
+		if err != nil {
+			return out, err
+		}
+
+		// AccountID
+		part3, err := strconv.ParseUint(parts[3], 10, 32)
+		if err != nil {
+			return out, err
+		}
+
+		account := (uint32(part3) << 1) | uint32(part2)
+
+		//
+		return NewPlayerID(UniverseID(i), AccountTypeIndividual, 1, AccountID(account)), nil
+
+	case regexpID3I.MatchString(id):
+
+		parts := regexpID3I.FindStringSubmatch(id)
+
+		// Account Type
+		var typex AccountType
+		var ok bool
+		if typex, ok = CharacterToAccountType[parts[1]]; !ok {
+			return out, ErrInvalidPlayerID
+		}
+
+		// Universe ID
+		part2, err := strconv.ParseUint(parts[2], 10, 8)
+		if err != nil {
+			return out, err
+		}
+
+		// Account ID
+		part3, err := strconv.ParseUint(parts[3], 10, 32)
+		if err != nil {
+			return out, err
+		}
+
+		// Instance ID
+		part4, err := strconv.ParseUint(parts[4], 10, 32)
+		if err != nil {
+			return out, err
+		}
+
+		//
+		return NewPlayerID(UniverseID(part2), typex, InstanceID(part4), AccountID(part3)), nil
+
+	case regexpID3.MatchString(id):
+
+		parts := regexpID3.FindStringSubmatch(id)
+
+		// Account Type
+		var typex AccountType
+		var ok bool
+		if typex, ok = CharacterToAccountType[parts[1]]; !ok {
+			return out, ErrInvalidPlayerID
+		}
+
+		// Universe ID
+		part2, err := strconv.ParseUint(parts[2], 10, 8)
+		if err != nil {
+			return out, err
+		}
+
+		// Account ID
+		part3, err := strconv.ParseUint(parts[3], 10, 32)
+		if err != nil {
+			return out, err
+		}
+
+		//
+		return NewPlayerID(UniverseID(part2), typex, 1, AccountID(part3)), nil
+
+	case regexpID32.MatchString(id):
+
+		i, err := strconv.ParseUint(id, 10, 32)
+		if err != nil {
+			return out, err
+		}
+
+		return NewPlayerID(UniversePublic, AccountTypeIndividual, 1, AccountID(i)), nil
+
+	case regexpID64.MatchString(id):
+
+		i, err := strconv.ParseUint(id, 10, 64)
+		if err != nil {
+			return out, err
+		}
+
+		return PlayerID(i), nil
+
+	default:
+
+		return out, ErrInvalidPlayerID
+	}
+}
+
+func NewPlayerID(universe UniverseID, accountType AccountType, instance InstanceID, accountId AccountID) (id PlayerID) {
+	id.SetAccountID(accountId)
+	id.SetInstanceID(instance)
+	id.SetUniverseID(universe)
+	id.SetAccountType(accountType)
+	return id
+}
+
+// Helpers
+func (id PlayerID) get(offset uint, mask uint64) uint64 {
+	return (uint64(id) >> offset) & mask
+}
+
+func (id *PlayerID) set(offset uint, mask, value uint64) {
+	*id = PlayerID((uint64(*id) & ^(mask << offset)) | (value&mask)<<offset)
+}
+
+// Account ID
+func (id PlayerID) GetAccountID() AccountID {
+	return AccountID(id.get(0, 0xFFFFFFFF))
+}
+
+func (id *PlayerID) SetAccountID(accountID AccountID) {
+	id.set(0, 0xFFFFFFFF, uint64(accountID))
+}
+
+// Instance ID
+func (id PlayerID) GetInstanceID() uint32 {
+	return uint32(id.get(32, 0xFFFFF))
+}
+
+func (id *PlayerID) SetInstanceID(instanceID InstanceID) {
+	id.set(32, 0xFFFFF, uint64(instanceID))
+}
+
+// Account Type
+func (id PlayerID) GetAccountType() AccountType {
+	return AccountType(id.get(52, 0xF))
+}
+
+func (id *PlayerID) SetAccountType(accountType AccountType) {
+	id.set(52, 0xF, uint64(accountType))
+}
+
+// Universe ID
+func (id PlayerID) GetUniverseID() UniverseID {
+	return UniverseID(id.get(56, 0xF))
+}
+
+func (id *PlayerID) SetUniverseID(universeID UniverseID) {
+	id.set(56, 0xF, uint64(universeID))
+}
+
+// Clan / Chat
+func (id PlayerID) ClanToChat() error {
+
+	if id.GetAccountType() != AccountTypeClan {
+		return ErrInvalidClanID
+	}
+
+	id.SetInstanceID(InstanceID(ChatInstanceClan))
+	id.SetAccountType(AccountTypeChat)
+
+	return nil
+}
+
+func (id PlayerID) ChatToClan() error {
+
+	if id.GetAccountType() != AccountTypeChat {
+		return ErrInvalidChatID
+	}
+
+	id.SetInstanceID(0)
+	id.SetAccountType(AccountTypeClan)
+
+	return nil
+}
---
+++ id/id_test.go
@@ -0,0 +1,54 @@
+package id
+
+import (
+	"testing"
+)
+
+func TestGettersSetters(t *testing.T) {
+
+	id := NewPlayerID(UniversePublic, AccountTypeIndividual, 1, 8360464)
+
+	if id != 76561197968626192 {
+		t.Error(id, 76561197968626192)
+	}
+
+	if id.GetUniverseID() != UniversePublic {
+		t.Error("GetUniverseID", id.GetUniverseID(), UniversePublic)
+	}
+
+	if id.GetAccountType() != AccountTypeIndividual {
+		t.Error("GetAccountType", id.GetAccountType(), AccountTypeIndividual)
+	}
+
+	if id.GetInstanceID() != 1 {
+		t.Error("GetInstanceID", id.GetInstanceID(), 1)
+	}
+
+	if id.GetAccountID() != 8360464 {
+		t.Error("GetAccountID", id.GetAccountID(), 4180232)
+	}
+}
+
+func TestIDTypes(t *testing.T) {
+
+	ids := []string{
+		"STEAM_0:0:4180232",
+		"U:1:8360464",
+		"[U:1:8360464]",
+		"U:1:8360464:1",
+		"[U:1:8360464:1]",
+		"8360464",
+		"76561197968626192",
+	}
+
+	for _, idIn := range ids {
+
+		id, err := ParsePlayerID(idIn)
+		if err != nil {
+			t.Error(err, idIn)
+		}
+		if id != 76561197968626192 {
+			t.Error("not me", idIn)
+		}
+	}
+}
